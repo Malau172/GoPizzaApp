77defad105363814b57751184c837aeeaa23273e
Malau172 <darosalaura83@gmail.com>
2022-06-09 23:05:42 -0300

    Button

--- GoPizzaApp/src/screens/Signin/index.tsx
+++ GoPizzaApp/src/screens/Signin/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from  "..//../components/Input/Button"
 import {Input} from "..//../components/Input";
 
 import { Container } from "./styles";
@@ -18,6 +19,11 @@ export function SignIn() {
         placeholder="Senha"
         type="secondary"
         secureTextEntry
+      />
+      
+      <Button
+        title= "Entrar"
+        type="primary"
       />
       
     </Container>
